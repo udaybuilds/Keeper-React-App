e0e5300884bbed2e61345ae12f18affe8c325d14
Uday <uday@dxc.com>
2021-08-31 23:54:16 +0530

    Updates

---
+++ precache-manifest.848fc1ce698a9fe6896efd0743d5a58f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2287b9ef0474369b32364ecbbd330183",
+    "url": "/Keeper-React-App/index.html"
+  },
+  {
+    "revision": "145f66d4b07df8da5033",
+    "url": "/Keeper-React-App/static/js/2.1e8d142e.chunk.js"
+  },
+  {
+    "revision": "2e607591aa854b60700f",
+    "url": "/Keeper-React-App/static/js/main.74b3a4e4.chunk.js"
+  },
+  {
+    "revision": "41a62db0136df36610e4",
+    "url": "/Keeper-React-App/static/js/runtime-main.8d688139.js"
+  }
+]);
